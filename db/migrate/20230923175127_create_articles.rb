95239f1e3274b1de4b990b84350f085bc9de1708
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-09-24 16:31:21 -0700

    add new users table to db

--- db/migrate/20230923175127_create_articles.rb
+++ db/migrate/20230923175127_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
     create_table :articles do |t|
       t.string :title
       t.text :description
-
+      t.user_id :int
       t.timestamps
     end
   end
