2c24050da7e2808dbc8f5089a5820228cae359b7
Felipe Ocampo <felipe.aocampo@gmail.com>
2023-09-24 16:49:40 -0700

    add email col to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_24_231202) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_24_233557) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_24_231202) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
